15c3f2d7cd07bc1c7eee44f6f89cc8e1f49f4e33
jmesquita1703 <mesquitta@gmail.com>
2015-02-18 12:37:25 -0200

    Projeto de demonstração do uso do Nhibernate
    Início do projeto

---
+++ Domain/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstSolution.Domain
+{
+    public class Product
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Category { get; set; }
+        public virtual bool Discontinued { get; set; }
+    }
+}
